b02aca078c28d3734bc12985b2f88f624c3171bd
Can <devecann@gmail.com>
2022-08-02 10:59:20 +0300

    lol

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="welcome">
+    <container>
+      WELCOME
+    </container>
+  </div>
+</template>
+
+<script>
+if (process.client) {
+  window.addEventListener('load', () => {
+    if (!('serviceWorker' in navigator)) {
+      throw new Error('serviceWorker is not supported in current browser!')
+    }
+    navigator.serviceWorker.register('/sw.js')
+  })
+}
+</script>
+
+<style>
+.welcome{
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  meta: {
+    title: 'DENEME APP',
+    charset: 'utf-8',
+    meta: [
+      { name: 'theme-color', content: '#ffdd67' }
+    ],
+    link: [
+      { hid: 'icon', rel: 'icon', type: 'image/png', href: '/logo_x32.png' },
+      { hid: 'apple-touch-icon', rel: 'apple-touch-icon', href: '/logo_x180.png' },
+      { rel: 'manifest', href: '/manifest.json' }
+    ]
+  }
+})
